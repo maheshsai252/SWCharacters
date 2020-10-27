754b47fe9aae89696f1ebc9b336fbd08ba4b6960
maheshsai252 <45325873+maheshsai252@users.noreply.github.com>
2020-10-27 12:33:19 +0530

    Initial Commit

---
+++ SW Characters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SW Characters
+//
+//  Created by Mahesh sai on 27/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SW CharactersTests/SW_CharactersTests.swift
@@ -0,0 +1,33 @@
+//
+//  SW_CharactersTests.swift
+//  SW CharactersTests
+//
+//  Created by Mahesh sai on 27/10/20.
+//
+
+import XCTest
+@testable import SW_Characters
+
+class SW_CharactersTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
